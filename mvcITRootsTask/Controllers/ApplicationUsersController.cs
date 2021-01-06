368b7aa25e28ef9e2bae0aae46b0b257f02c49ef
hasnaaahmedd123@gmail.com <hasnaaahmedd123@gmail.com>
2021-01-06 19:20:49 +0200

    first commit

---
+++ mvcITRootsTask/Controllers/ApplicationUsersController.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using mvcITRootsTask.Models;
+using mvcITRootsTask.UsersReport;
+
+namespace mvcITRootsTask.Controllers
+{
+    [Authorize]
+
+    public class ApplicationUsersController : Controller
+    {
+        private ApplicationUserManager _userManager;
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ApplicationUsersController()
+        {
+            
+
+        }
+
+        public ApplicationUsersController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+            
+        }
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        // GET: ApplicationUsers
+        public ActionResult Index(string searching)
+        {
+            return View(db.Users.Where(u=> u.UserName.Contains(searching)||searching==null).ToList());
+        }
+
+        //Download PDF
+
+        public ActionResult DownloadPDF()
+        {
+            UserReport userReport = new UserReport();
+
+            byte[] abytes = userReport.PrepareReport(db.Users.ToList());
+
+            return File(abytes, "application/pdf");
+        }
+
+        // GET: ApplicationUsers/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser applicationUser = db.Users.Find(id);
+            if (applicationUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicationUser);
+        }
+
+        // GET: ApplicationUsers/Create
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        // POST: ApplicationUsers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(ApplicationUser applicationUser)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { FullName = applicationUser.FullName,
+                    UserName = applicationUser.Email,
+                    Email = applicationUser.Email,
+                    PhoneNumber = applicationUser.PhoneNumber };
+
+                var result = await UserManager.CreateAsync(user, applicationUser.PasswordHash);
+
+                if(result.Succeeded)
+                {
+                    await UserManager.AddToRoleAsync(user.Id, "user");
+
+                    //db.Users.Add(applicationUser);
+                    //db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                
+            }
+
+            return View(applicationUser);
+        }
+
+        // GET: ApplicationUsers/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser applicationUser = db.Users.Find(id);
+            if (applicationUser == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(applicationUser);
+        }
+
+        // POST: ApplicationUsers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FullName,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] ApplicationUser applicationUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(applicationUser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(applicationUser);
+        }
+
+        // GET: ApplicationUsers/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser applicationUser = db.Users.Find(id);
+            if (applicationUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicationUser);
+        }
+
+        // POST: ApplicationUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            ApplicationUser applicationUser = db.Users.Find(id);
+            db.Users.Remove(applicationUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ mvcITRootsTask/UsersReport/UserReport.cs
@@ -0,0 +1,111 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using mvcITRootsTask.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace mvcITRootsTask.UsersReport
+{
+    public class UserReport
+    {
+        //decleration
+        int _totalColumn = 3;
+        Document _document;
+        Font _fontStyle;
+        PdfPTable _pdfPTable = new PdfPTable(3);
+        PdfPCell _pdfpCell;
+        MemoryStream _memoryStream = new MemoryStream();
+        List<ApplicationUser> _users = new List<ApplicationUser>();
+
+        //functions
+        
+        public byte[] PrepareReport(List<ApplicationUser> users)
+        {
+            _users = users;
+            _document = new Document(PageSize.A4, 0f, 0f, 0f, 0f);
+            _document.SetPageSize(PageSize.A4);
+            _document.SetMargins(20f, 20f, 20f, 20f);
+            _pdfPTable.WidthPercentage = 100;
+            _pdfPTable.HorizontalAlignment = Element.ALIGN_LEFT;
+            _fontStyle = FontFactory.GetFont("Tahoma", 8f, 1);
+            PdfWriter.GetInstance(_document, _memoryStream);
+            _document.Open();
+            _pdfPTable.SetWidths(new float[] { 100f, 100f, 100f });
+
+
+            this.ReportHeader();
+            this.ReportBody();
+            _pdfPTable.HeaderRows = 2;
+            _document.Add(_pdfPTable);
+            _document.Close();
+
+            return _memoryStream.ToArray();
+        }
+
+        private void ReportHeader()
+        {
+            _fontStyle = FontFactory.GetFont("Tahoma", 11f, 1);
+            _pdfpCell = new PdfPCell(new Phrase("Report", _fontStyle));
+            _pdfpCell.Colspan = _totalColumn;
+            _pdfpCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfpCell.Border = 0;
+            _pdfpCell.BackgroundColor = BaseColor.WHITE;
+            _pdfpCell.ExtraParagraphSpace = 0;
+            _pdfPTable.AddCell(_pdfpCell);
+            _pdfPTable.CompleteRow();
+        }
+
+        private void ReportBody()
+        {
+            //table header
+            _fontStyle = FontFactory.GetFont("Tahoma", 8f, 1);
+            _pdfpCell = new PdfPCell(new Phrase("UserName", _fontStyle));
+            _pdfpCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfpCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfpCell.BackgroundColor = BaseColor.GRAY;
+            _pdfPTable.AddCell(_pdfpCell);
+
+            _fontStyle = FontFactory.GetFont("Tahoma", 8f, 1);
+            _pdfpCell = new PdfPCell(new Phrase("Phone Number", _fontStyle));
+            _pdfpCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfpCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfpCell.BackgroundColor = BaseColor.GRAY;
+            _pdfPTable.AddCell(_pdfpCell);
+
+            _pdfpCell = new PdfPCell(new Phrase("Email", _fontStyle));
+            _pdfpCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfpCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfpCell.BackgroundColor = BaseColor.GRAY;
+            _pdfPTable.AddCell(_pdfpCell);
+            _pdfPTable.CompleteRow();
+
+            //table body
+
+            _fontStyle = FontFactory.GetFont("Tahoma", 8f, 1);
+            foreach(ApplicationUser users in _users)
+            {
+                _pdfpCell = new PdfPCell(new Phrase(users.UserName, _fontStyle));
+                _pdfpCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfpCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfpCell.BackgroundColor = BaseColor.GRAY;
+                _pdfPTable.AddCell(_pdfpCell);
+
+                _pdfpCell = new PdfPCell(new Phrase(users.PhoneNumber, _fontStyle));
+                _pdfpCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfpCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfpCell.BackgroundColor = BaseColor.GRAY;
+                _pdfPTable.AddCell(_pdfpCell);
+
+                _pdfpCell = new PdfPCell(new Phrase(users.Email, _fontStyle));
+                _pdfpCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfpCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfpCell.BackgroundColor = BaseColor.GRAY;
+                _pdfPTable.AddCell(_pdfpCell);
+                _pdfPTable.CompleteRow();
+            }
+        }
+    }
+}
